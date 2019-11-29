1fd8aee1ecf76aa513bc63956cbaa138adbaeafc
Eugene Antelava <antelavaen@gmail.com>
2019-11-29 15:11:52 +0300

    First commit

---
+++ src/main/java/com/jaxwstest/JaxWsTestApplication.java
@@ -0,0 +1,13 @@
+package com.jaxwstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxWsTestApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JaxWsTestApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/jaxwstest/JaxWsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaxwstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxWsTestApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
